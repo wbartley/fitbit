9421b6b398ce6c8b1a72f1575dec281e406426bb
William <william@solesociety.com>
2013-08-16 20:29:31 -0700

    Add https

--- fitbit_server.js
+++ fitbit_server.js
@@ -1,14 +1,17 @@
 Fitbit.whitelistedFields = ['encodedId', 'displayName'];
 
 Oauth.registerService('fitbit', 1, Fitbit._urls, function(oauthBinding) {
-  var identity = oauthBinding.get('http://api.fitbit.com/1/user/-/profile.json').data;
+  var identity = oauthBinding.get('https://api.fitbit.com/1/user/-/profile.json').data;
 
   var serviceData = {
     id: identity.user.encodedId,
-    displayName: identity.user.displayName,
+    name: identity.user.displayName,
     accessToken: oauthBinding.accessToken,
     accessTokenSecret: oauthBinding.accessTokenSecret
   };
+
+	var profile = identity.user; //add all fitbit profile data
+	profile["name"] = identity.user.displayName; //add name to see in default login buttons
 
   // include helpful fields from Fitbit
   var fields = _.pick(identity, Fitbit.whitelistedFields);
@@ -17,10 +20,8 @@ Oauth.registerService('fitbit', 1, Fitbit._urls, function(oauthBinding) {
   return {
     serviceData: serviceData,
     options: {
-      profile: {
-        name: identity.user.fullName
-      }
-    }
+    	profile: profile
+	}
   };
 });
 
